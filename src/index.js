f41811bc44332dc398b298afc00ca21fce74455d
heintzz <mhasnanr@gmail.com>
2022-07-12 10:11:55 +0700

    setting up firebase

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyDombaZ9iIZv8yrvNNGfI1dxb7klGHmMIY',
+    authDomain: 'fir-v9-6a418.firebaseapp.com',
+    projectId: 'fir-v9-6a418',
+    storageBucket: 'fir-v9-6a418.appspot.com',
+    messagingSenderId: '734596388116',
+    appId: '1:734596388116:web:8116e8ca706bf7cff55e01',
+}
+
+initializeApp(firebaseConfig)
