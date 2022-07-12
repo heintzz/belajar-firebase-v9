faa814b30fba78a7e13df5c4c75463f0137b7766
heintzz <mhasnanr@gmail.com>
2022-07-12 13:13:16 +0700

    created authentication

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import { initializeApp } from 'firebase/app'
 import {
-    getFirestore, collection, getDocs, onSnapshot,
-    addDoc, deleteDoc, doc,
-    query, where, 
-    orderBy, serverTimestamp,
-    getDoc 
+    getFirestore,
+    collection,
+    getDocs,
+    onSnapshot,
+    addDoc,
+    deleteDoc,
+    doc,
+    query,
+    where,
+    orderBy,
+    serverTimestamp,
+    getDoc,
+    updateDoc,
 } from 'firebase/firestore'
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signOut,
+    signInWithEmailAndPassword,
+    onAuthStateChanged
+} from 'firebase/auth'
 
 const firebaseConfig = {
     apiKey: 'AIzaSyDombaZ9iIZv8yrvNNGfI1dxb7klGHmMIY',
@@ -21,6 +36,7 @@ initializeApp(firebaseConfig)
 
 // init services
 const db = getFirestore()
+const auth = getAuth()
 
 // collection reference
 const colRef = collection(db, 'books')
@@ -28,7 +44,6 @@ const colRef = collection(db, 'books')
 // queries
 // 2nd params -> where('author', '==', 'hasnan regard')
 const q = query(colRef, orderBy('createdAt'))
-
 
 // getDocs(colRef)
 //     .then((snapshot) => {
@@ -88,3 +103,63 @@ const docRef = doc(db, 'books', 'CyvE8b1G35EDYo9STO35')
 onSnapshot(docRef, (doc) => {
     console.log(doc.data(), doc.id)
 })
+
+// update properties on a single document
+const updateForm = document.querySelector('.update')
+updateForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const docRef = doc(db, 'books', updateForm.id.value)
+    updateDoc(docRef, {
+        title: 'updated title',
+    }).then(() => {
+        updateForm.reset()
+    })
+})
+
+// signing users up
+const signupForm = document.querySelector('.signup')
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const email = signupForm.email.value
+    const password = signupForm.password.value
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((cred) => {
+            // console.log('user created: ', cred.user)
+            signupForm.reset()
+        })
+        .catch((err) => {
+            console.log(err.message)
+        })
+})
+
+const loginButton = document.querySelector('.login')
+loginButton.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const email = signupForm.email.value
+    const password = signupForm.password.value
+
+    signInWithEmailAndPassword(auth, email, password).then((cred) => {
+        // console.log('user logged in: ', cred.user)
+    }).catch((err) => {
+        console.log(err.message)
+    })
+})
+
+const logoutButton = document.querySelector('.logout')
+logoutButton.addEventListener('click', () => {
+    signOut(auth)
+        .then(() => {
+            // console.log('the user logged out')
+        })
+        .catch((err) => {
+            console.log(err.message)
+        })
+})
+
+// subscribing to auth changes
+onAuthStateChanged(auth, (user) => {
+    console.log('user status changed: ', user)
+})
+
+// unsubscirbing from changes
